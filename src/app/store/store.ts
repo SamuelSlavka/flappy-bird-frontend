c621a9646f37dbd133d92852a406789aa7d18be7
samuelslavka <slavka.samuel@gmail.com>
2022-12-26 20:11:56 +0100

    daysiui theme fix for chrome

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -4,34 +4,41 @@ import playerReducer from '../pages/GamePage/store/playerSlice';
 import loginReducer from '../pages/LoginPage/store/loginSlice';
 import storage from 'redux-persist/lib/storage';
 import {
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
 } from 'redux-persist';
 
 const persistConfig = {
-  key: 'player',
-  storage
-}
+    key: 'player',
+    storage,
+};
 
 const playerPeristReducer = persistReducer(persistConfig, playerReducer);
 
 const store = configureStore({
-  reducer: {
-    player: playerPeristReducer,
-    login: loginReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-})
+    reducer: {
+        player: playerPeristReducer,
+        login: loginReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,4 +17,13 @@ module.exports = {
 
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    themes: ["synthwave"],
+  },
 }
